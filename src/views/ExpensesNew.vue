7678cb1f6d3314efa0dd851c3f0a2d6dc8372a19
sdimichele <sabrinadimichele1992@gmail.com>
2019-06-17 15:29:32 -0500

    expenses new changes, getting it to talk to git

--- src/views/ExpensesNew.vue
+++ src/views/ExpensesNew.vue
@@ -1,68 +1,75 @@
 <template>
   <div class="container">
     <h1>New Expense</h1>
+     <ul>
+      <li v-for="error in errors">
+        {{ error }}
+      </li>
+    </ul>
     <div>
       Name: <input type="text" v-model="newExpenseName">
       Amount: <input type="text" v-model="newExpenseAmount">
-      Comment: <input type="text" v-model="newExpenseComment">
-      Cattegory: <input type="text" v-model="category.id">
-      
-<!-- 
-      <div v-for="post in posts">
-        <img class="index-posts-img" v-bind:src="post.image" v-bind:alt="post.title">
-        <h2><router-link v-bind:to="'/posts/' + post.id">{{post.title}}</router-link></h2>
-    </div> -->
+      Comment: <input type="text" v-model="newExpenseComment"> 
+
+  <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Dropdown button
+    </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <div v-for="category in user.categories">
+          <a v-on:click="newExpenseCategoryId = category.id" class="dropdown-item" href="#">{{category.name}}</a>
+        </div>
+      </div>
+
+  </div>
+
 
       <button v-on:click="createExpense()">Create Expense</button>
-    </div>
-    <h1>All Expenses</h1>
-    <div v-for="expense in expenses">
-      <h4>{{ expense.name }}</h4>
-      <h4> {{expense.category_name}} </h4>
-      <h4> {{expense.amount}}</h4>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-
 export default {
   data: function() {
     return {
       expenses: [],
-      categories: [],
       currentExpense: {},
+      user: {},
       newExpenseName: "",
       newExpenseAmount: "",
       newExpenseComment: "",
-      newExpenseCategory: ""
+      newExpenseCategoryId: "",
+      errors: []
     };
   },
   created: function() {
     axios.get("/api/expenses").then(response => {
       this.expenses = response.data;
     });
-    axios.get("/api/expenses").then(response => {
-      this.expenses = response.data;
+    axios.get("/api/users").then(response => {
+      this.user = response.data;
     });
   },
   methods: {
+    // createCat: function(cat) {
+    //   this.newExpenseCategory = cat
+    // };
+
     createExpense: function() {
       var params = {
-        Name: this.newExpenseName,
-        Amount: this.newExpenseAmount,
-        Comment: this.newExpenseComment,
-        Category: this.newExpenseCategory
+        name: this.newExpenseName,
+        amount: this.newExpenseAmount,
+        comment: this.newExpenseComment,
+        category_id: this.newExpenseCategoryId
       };
       axios.post("/api/expenses", params).then(response => {
-        this.newExpenseName= "";
-        this.newExpenseAmount = "";
-        this.newExpenseComment = "";
-        this.newExpenseCategory = "";
-      });
+        this.$router.push("/");
+        }).catch(error => {
+          this.errors = error.response.data.errors;
+        });
     }
-
   }
 };
 </script>
